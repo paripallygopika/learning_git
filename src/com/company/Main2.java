4c3d1add176ba426eb9763bb71ae9d765106cc4f
paripallygopika <112694867+paripallygopika@users.noreply.github.com>
2022-09-23 16:51:27 +0530

    Update Main2.java
    gopika_git_remote

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -4,6 +4,7 @@ public class Main2 {
     public static void main(String[] args) {
         // write your code here
         System.out.println("main2");
+        // made changes _ paripallygopika_git_remote!!!
     }
 
 }
